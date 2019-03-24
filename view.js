7ef0ac6fbde85a8865c74a280671064a6213704c
Cynthia Randriamanohisoa <CynthiaR@BitTitan.com>
2019-03-23 18:45:17 -0700

    Cleanup UI and show estimate

--- view.js
+++ view.js
@@ -1,4 +1,3 @@
-// const distanceUrl = 'https://jsonplaceholder.typicode.com/todos/1'
 const originAddress = '1120 112th Ave NE Bellevue WA 98004'
 const destinationAddress = '8001 Greenwood Ave N, Seattle, WA 98103' // ADI
 const apiKey = 'AIzaSyAciWtCnB8EdadekShFPBzCirE065e2inQ'
@@ -22,16 +21,62 @@ var distanceQueryParams = {
 };
 
 let $ = require('jquery')  // jQuery now loaded and assigned to $
-let count = 0
-$('#click-counter').text(count.toString())
-$('#countbtn').on('click', () => {
-   count ++ 
-   $('#click-counter').text(count)
-   
+let estimatedTime = "--"
+$('#distance-result').text(estimatedTime)
+$('#check-button').on('click', () => {   
    var distanceQuery = distanceUrl + '?' + $.param(distanceQueryParams)
    
-   $.get(distanceQuery, function(result) {
-    console.log(result)   
-    $('#distance-result').text(result)
+   var distanceEstimateResult = {}
+   fetch(distanceQuery)
+   .then(response => {
+        if (response.ok) {
+            return response.json()
+        }            
+        else {
+            throw new Error("Failed to connect to distance API.")
+        }
+            
    })
-})
+   .then(function(responseJson) {
+       console.log(responseJson)
+       distanceEstimateResult = responseJson
+   
+       var elements = distanceEstimateResult.rows[0].elements
+       var estimatedTime = elements[0].duration_in_traffic.text
+       $('#distance-result').text(estimatedTime)
+    })
+   .catch(function(error) {
+       console.log(error.message)
+   })
+
+})  
+
+/*
+{
+   "destination_addresses" : [ "8001 Greenwood Ave N, Seattle, WA 98103, USA" ],
+   "origin_addresses" : [ "1120 112th Ave NE, Bellevue, WA 98004, USA" ],
+   "rows" : [
+      {
+         "elements" : [
+            {
+               "distance" : {
+                  "text" : "20.7 km",
+                  "value" : 20722
+               },
+               "duration" : {
+                  "text" : "20 mins",
+                  "value" : 1196
+               },
+               "duration_in_traffic" : {
+                  "text" : "22 mins",
+                  "value" : 1299
+               },
+               "status" : "OK"
+            }
+         ]
+      }
+   ],
+   "status" : "OK"
+}
+
+*/
